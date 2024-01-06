c02b08e363e5e661f68929d58eb89ffc9b006ae0
Dariga5 <isa2407@mail.ru>
2024-01-07 01:56:53 +0500

    dev

--- utils/handlers.go
+++ utils/handlers.go
@@ -1,41 +1,13 @@
 package utils
 
 import (
-	"fmt"
-	"io"
-	"log"
 	"net/http"
-	"os"
 )
 
 func StartHandle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello! Send file to address: POST /api/v1/sendfile")
+	http.FileServer(http.Dir("../static"))
 
 }
 func FileHandler(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method != "POST" {
-		http.Error(w, "Wethod not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	file, header, err := r.FormFile("file")
-
-	log.Print(header)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	defer file.Close()
-
-	dst, err := os.Create("./uplodes")
-
-	if err != nil {
-		return
-	}
-
-	io.Copy(dst, file)
-	fmt.Fprintf(w, "Upload succesful")
 }
